461238edd6513bd3856cbcf167c6618b7b50f649
trungdq2407 <trungdq2407@gmail.com>
2023-12-19 18:32:20 +0700

    add user popup

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,10 +1,11 @@
 'use client'
+import AddUser from '@/components/User/addUser/addUser';
+import DeleteUser from '@/components/User/deleteUser/deleteUser';
 import ButtonComponent from '@/constants/Button/Button';
 import Header from '@/constants/Header/Header';
 import Sidebar from '@/constants/Sidebar/Sidebar';
 import DefaultLayout from '@/layouts/User/DefaultLayout';
 import { useState } from 'react';
->>>>>>> 80d8ad50d4ef0b35053b9eb47680f00b73761391
 
 
 export default function Index() {
