13dad3f6f341fc794a3ed4ca6f58a589a74d9ca5
Billy <billy@vitex.asia>
2023-12-21 18:06:58 +0700

    Change layout work

--- src/app/[locale]/(logged-in)/company/page.tsx
+++ src/app/[locale]/(logged-in)/company/page.tsx
@@ -6,7 +6,7 @@ import datas from "./datatable";
 import Button from "@/constants/Form/Button";
 
 const CompanyList = () => {
-    const columns =[
+    const columns = [
         'No',
         'Company_Name',
         'Company_Domain',
@@ -20,26 +20,24 @@ const CompanyList = () => {
         <button key="eye" className={styles.custombutton}><img src="/eye.svg" ></img></button>,
         <button key="skipdownline" className={styles.custombutton}><img src="/skipdownline.svg"></img></button>,
         <button key="edit" className={styles.custombutton}><img src="/edit.svg"></img></button>,
-        <button key="delete" className={styles.custombutton} style={{backgroundColor:'#E56353'}}><img src="/delete.svg"></img></button>,
-      ];
-    return(
-        <DefaultLayout>
-            <div className={styles.container}>
-                <div className={styles.labelsection}>
-                    <div className={styles.square}>
-                    </div>
-                    <h1 className={styles.label}>Company List</h1>
+        <button key="delete" className={styles.custombutton} style={{ backgroundColor: '#E56353' }}><img src="/delete.svg"></img></button>,
+    ];
+    return (
+        <div className={styles.container}>
+            <div className={styles.labelsection}>
+                <div className={styles.square}>
                 </div>
-                <div className={styles.companytable}>
-                    <Table data={datas} columns={columns} actions={actions}>
-                        
-                    </Table>
-                </div>
-                <div className={styles.addco}>
-                    <Button className={styles.addbtn}>ADD NEW COMPANY</Button>
-                </div>
+                <h1 className={styles.label}>Company List</h1>
             </div>
-        </DefaultLayout>
+            <div className={styles.companytable}>
+                <Table data={datas} columns={columns} actions={actions}>
+
+                </Table>
+            </div>
+            <div className={styles.addco}>
+                <Button className={styles.addbtn}>ADD NEW COMPANY</Button>
+            </div>
+        </div>
     );
 }
 export default CompanyList
