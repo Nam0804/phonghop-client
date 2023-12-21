214a816e7bb3904dc17aef53498ccc75f98ce1b1
Billy <billy@vitex.asia>
2023-12-21 17:09:59 +0700

    Minor pathing changes

--- src/app/[locale]/(auth)/user/login/page.tsx
+++ src/app/[locale]/(auth)/user/login/page.tsx
@@ -2,7 +2,7 @@
 import * as React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Input from '@/constants/Form/Input';
-import styles from 'cssPath/Login.module.css';
+import styles from '@/css/Login.module.css';
 import DefaultLoginLayout from '@/layouts/User/DefaultLoginLayout';
 import Checkbox, { CheckboxChangeEvent } from 'antd/es/checkbox/Checkbox';
 import Link from 'next/link';
