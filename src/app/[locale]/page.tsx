23e21eac08473b04b5739f779707e589ba13b93f
trungdq2407 <trungdq2407@gmail.com>
2023-12-24 18:28:02 +0700

    edit user update

--- src/app/[locale]/page.tsx
+++
@@ -1,7 +0,0 @@
-'use client'
-import ButtonComponent from '@/constants/Button/Button';
-import Header from '@/constants/Header/Header';
-import Sidebar from '@/constants/Sidebar/Sidebar';
-import DefaultLayout from '@/layouts/User/DefaultLayout';
-import { useState } from 'react';
-
