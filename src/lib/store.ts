da74fc8add08cfe4f6937702a9cbd0bd3f88a348
BinPhan <binalwayssmile@gmail.com>
2024-01-03 16:52:50 +0700

    Add presist

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,17 +1,35 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import userReducer from './features/user/userSlice'
 import loadingReducer from './features/loadingSlice'
+import profileReducer from './features/profile/profileSlice'
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+import { store } from '@/redux/store';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  loading: loadingReducer,
+  profile: profileReducer
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 export const makeStore = () => {
   return configureStore({
-    reducer: {
-      user: userReducer,
-      loading: loadingReducer,
-    }
+    reducer: persistedReducer
   })
 }
+
 
 // Infer the type of makeStore
 export type AppStore = ReturnType<typeof makeStore>
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<AppStore['getState']>
 export type AppDispatch = AppStore['dispatch']
+
+export const persistor = persistStore(makeStore())
