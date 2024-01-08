d60f242432e25dd26a98eb6cfbb4f4e1e1406662
AN Nguyen <andepzaiquadepzai@gmail.com>
2024-01-08 16:48:31 +0700

    ok

--- src/constants/Sidebar/Sidebar.tsx
+++ src/constants/Sidebar/Sidebar.tsx
@@ -10,11 +10,14 @@ import Profile from "@/components/User/profile";
 import { useSelector } from 'react-redux';
 import { useAppDispatch } from '@/lib/hooks';
 import { setLoading } from '@/lib/features/loadingSlice';
+import { useLocale, useTranslations } from 'next-intl';
 
 const Sidebar = () => {
   const user = useSelector((state: any) => state.user.value);
   const usertype = user.type;
   const dispatch = useAppDispatch();
+  const locale = useLocale();
+
 
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isProfileOpen, setIsProfileOpen] = useState(false);
@@ -36,7 +39,7 @@ const Sidebar = () => {
       <ul className={styles.listsidebar}>
         {usertype === 0 && (
           <>
-            <Link href={"/vn/company"} className={styles.customlink}>
+            <Link href={`/${locale}/company`} className={styles.customlink}>
               <li className={styles.sidebarItem}>
                 <Image src="/company.svg" alt="Company logo" width={'32'} height={'32'} />
                 <span className={styles.sidebarText}>Company</span>
@@ -46,19 +49,19 @@ const Sidebar = () => {
         )}
         {usertype === 1 && (
           <>
-            <Link href={"/vn/room"} className={styles.customlink}>
+            <Link href={`/${locale}/room`} className={styles.customlink}>
               <li className={styles.sidebarItem}>
                 <Image src="/room.svg" alt="Room logo" width={'32'} height={'32'} />
                 <span className={styles.sidebarText}>Room</span>
               </li>
             </Link>
-            <Link href={"/vn/booking"} className={styles.customlink}>
+            <Link href={`/${locale}/booking`} className={styles.customlink}>
               <li className={styles.sidebarItem}>
                 <Image src="/booking.svg" alt="Booking logo" width={'32'} height={'32'} />
                 <span className={styles.sidebarText}>Booking</span>
               </li>
             </Link>
-            <Link href={"/vn/users"} className={styles.customlink}>
+            <Link href={`/${locale}/users`} className={styles.customlink}>
               <li className={styles.sidebarItem}>
                 <Image src="/user.svg" alt="User logo" width={'32'} height={'32'} />
                 <span className={styles.sidebarText}>Users</span>            
@@ -68,13 +71,13 @@ const Sidebar = () => {
         )}
         {usertype === 2 && (
           <>
-            <Link href={"/vn/room"} className={styles.customlink}>
+            <Link href={`/${locale}/room`} className={styles.customlink}>
               <li className={styles.sidebarItem}>
                 <Image src="/room.svg" alt="Room logo" width={'32'} height={'32'} />
                 <span className={styles.sidebarText}>Room</span>                
               </li>
             </Link>
-            <Link href={"/vn/booking"} className={styles.customlink}>
+            <Link href={`/${locale}/booking`} className={styles.customlink}>
               <li className={styles.sidebarItem}>
                 <Image src="/booking.svg" alt="Booking logo" width={'32'} height={'32'} />
                 <span className={styles.sidebarText}>Users</span>  
@@ -84,7 +87,7 @@ const Sidebar = () => {
         )}
         {usertype === 3 && (
           <>
-            <Link href={"/vn/room"} className={styles.customlink}>
+            <Link href={`/${locale}/room`} className={styles.customlink}>
               <li className={styles.sidebarItem}>
                 <Image src="/room.svg" alt="Room logo" width={'32'} height={'32'} />
                 <span className={styles.sidebarText}>Room</span>                
