a182d6e12bffd62ca958ef19125c7da1a8b10b7f
trungdq2407 <trungdq2407@gmail.com>
2024-01-18 18:27:43 +0700

    room daily weekly update

--- src/components/Booking/RoomDetailWeekly.tsx
+++ src/components/Booking/RoomDetailWeekly.tsx
@@ -15,12 +15,18 @@ import type {DatePickerProps} from 'antd';
 import {DatePicker, Space} from 'antd';
 import {useSelector} from 'react-redux'
 import toast from "react-hot-toast";
-
+import DeleteUser from "@/components/User/deleteUser/deleteUser";
 const RoomDetailWeekly = () => {
-    const [title, settitle] = useState(new moment().format("MMMM DD, YYYY"));
+    interface Room {
+        id: number;
+        name: string;
+    }
+
+    const dateFormatforButtonChangeWeek = moment().format("MMMM DD, YYYY");
+    const [title, settitle] = useState<string>(dateFormatforButtonChangeWeek);
     const calendarRef = useRef<FullCalendar>(null);
     const user = useSelector((state: any) => state.user.value);
-    const [roomList, setRoomList] = useState([]);
+    const [roomList, setRoomList] = useState<Room[]>([]);
     const [selectedRoom, setSelectedRoom] = useState('');
     const [selectedCheckbox, setSelectedCheckbox] = useState('allMeetings');
     const [initialCheckbox, setInitialCheckbox] = useState('allMeetings');
@@ -52,24 +58,24 @@ const RoomDetailWeekly = () => {
     const fetchData = async () => {
         if (selectedCheckbox === 'myMeeting') {
             const myBookings = await fetchMyBookingHistory();
-            const eventMyBooking = myBookings.data.map((booking) => ({
+            const eventMyBooking = myBookings.map((booking:any) => ({
                 title: booking.topic,
                 start: booking.from_time,
                 end: booking.to_time,
                 allDay: false,
                 backgroundColor: '#388697',
-                booking_user: booking.username
+                booking_user: user.name
             }));
             setEvents(eventMyBooking);
         } else {
             const allBookings = await fetchAllBookingHistory();
-            const eventAllBooking = allBookings.map((booking) => ({
+            const eventAllBooking = allBookings.map((booking: any) => ({
                 title: booking.topic,
                 start: booking.from_time,
                 end: booking.to_time,
                 allDay: false,
                 backgroundColor: '#388697',
-                booking_user: booking.username
+                booking_user: user.name
             }));
             setEvents(eventAllBooking);
         }
@@ -84,7 +90,6 @@ const RoomDetailWeekly = () => {
         try {
             const response = await api.get(`/bookings/history/${user.id}`);
             const myBookings = get(response, 'data.data', []);
-            console.log(myBookings)
             return myBookings;
         } catch (error) {
             console.error(error);
@@ -96,7 +101,6 @@ const RoomDetailWeekly = () => {
         try {
             const response = await api.get(`/bookings`);
             const allBookings = get(response, 'data.data', []);
-            console.log(allBookings)
             return allBookings;
         } catch (error) {
             console.error(error);
@@ -148,30 +152,35 @@ const RoomDetailWeekly = () => {
 
         return `${dayOfWeek} ${formattedDate}`;
     };
-    const calendarStyle = (date: any) => {
-        let currentDate = `${new Date().getDate()} ${new Date().getMonth() + 1} ${new Date().getFullYear()}`
-        let allDate = `${date.getDate()} ${date.getMonth() + 1} ${date.getFullYear()}`
+    // const calendarStyle = (date: any) => {
+    //     let currentDate = `${new Date().getDate()} ${new Date().getMonth() + 1} ${new Date().getFullYear()}`
+    //     let allDate = `${date.getDate()} ${date.getMonth() + 1} ${date.getFullYear()}`
+    //
+    //     if (allDate === currentDate)
+    //         return {
+    //             style: {
+    //                 backgroundColor: '#88C9E8',
+    //                 border: '1px solid gray',
+    //                 margin: 0,
+    //                 padding: 0
+    //             }
+    //         }
+    // }
 
-        if (allDate === currentDate)
-            return {
-                style: {
-                    backgroundColor: '#88C9E8',
-                    border: '1px solid gray',
-                    margin: 0,
-                    padding: 0
-                }
-            }
-    }
-
-    const renderEventContent = (eventInfo) => {
+    const renderEventContent = (eventInfo: any) => {
         return (
             <>
-                <p><strong>{eventInfo.event.title}</strong></p>
-                <p>Booked by Kim: {eventInfo.event.extendedProps.booking_user}</p>
-                <b>{eventInfo.timeText}</b>
+                <p style={{marginBottom: '0px'}}><strong>Meeting: {eventInfo.event.title}</strong></p>
+                <p style={{marginBottom: '0px'}}>Booked by {eventInfo.event.extendedProps.booking_user}</p>
+                <p>{eventInfo.timeText}</p>
             </>
         )
     };
+
+    const showModalBooking = (e) => {
+        console.log(e);
+        <DeleteUser />
+    }
 
     return (
         <>
@@ -197,7 +206,7 @@ const RoomDetailWeekly = () => {
                                              viewBox="0 0 24 24"
                                              fill="none"
                                              style={{padding: "5px"}}>
-                                            <path fill-rule="evenodd" clip-rule="evenodd"
+                                            <path fillRule="evenodd" clipRule="evenodd"
                                                   d="M8.29303 12.7073C8.10556 12.5198 8.00024 12.2655 8.00024 12.0003C8.00024 11.7352 8.10556 11.4809 8.29303 11.2933L13.95 5.63634C14.0423 5.54083 14.1526 5.46465 14.2746 5.41224C14.3966 5.35983 14.5279 5.33225 14.6606 5.33109C14.7934 5.32994 14.9251 5.35524 15.048 5.40552C15.1709 5.4558 15.2825 5.53006 15.3764 5.62395C15.4703 5.71784 15.5446 5.82949 15.5949 5.95239C15.6451 6.07529 15.6704 6.20696 15.6693 6.33974C15.6681 6.47252 15.6405 6.60374 15.5881 6.72575C15.5357 6.84775 15.4595 6.9581 15.364 7.05034L10.414 12.0003L15.364 16.9503C15.5462 17.1389 15.647 17.3915 15.6447 17.6537C15.6424 17.9159 15.5373 18.1668 15.3518 18.3522C15.1664 18.5376 14.9156 18.6427 14.6534 18.645C14.3912 18.6473 14.1386 18.5465 13.95 18.3643L8.29303 12.7073Z"
                                                   fill="white"/>
                                         </svg>
@@ -215,7 +224,7 @@ const RoomDetailWeekly = () => {
                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                              viewBox="0 0 24 24"
                                              fill="none">
-                                            <path fill-rule="evenodd" clip-rule="evenodd"
+                                            <path fillRule="evenodd" clipRule="evenodd"
                                                   d="M15.7071 11.2932C15.8946 11.4807 15.9999 11.735 15.9999 12.0002C15.9999 12.2653 15.8946 12.5197 15.7071 12.7072L10.0501 18.3642C9.95785 18.4597 9.84751 18.5359 9.7255 18.5883C9.6035 18.6407 9.47228 18.6683 9.3395 18.6694C9.20672 18.6706 9.07504 18.6453 8.95215 18.595C8.82925 18.5447 8.7176 18.4705 8.6237 18.3766C8.52981 18.2827 8.45556 18.171 8.40528 18.0481C8.355 17.9252 8.32969 17.7936 8.33085 17.6608C8.332 17.528 8.35959 17.3968 8.412 17.2748C8.46441 17.1528 8.54059 17.0424 8.6361 16.9502L13.5861 12.0002L8.6361 7.05018C8.45394 6.86158 8.35315 6.60898 8.35542 6.34678C8.3577 6.08458 8.46287 5.83377 8.64828 5.64836C8.83369 5.46295 9.0845 5.35778 9.3467 5.35551C9.60889 5.35323 9.8615 5.45402 10.0501 5.63618L15.7071 11.2932Z"
                                                   fill="white"/>
                                         </svg>
@@ -303,16 +312,23 @@ const RoomDetailWeekly = () => {
                                 headerToolbar={false}
                                 allDaySlot={false}
                                 editable
-                                selectable
-                                selectOverlap={false}
+                                selectable={true}
+                                selectMirror={true}
+                                selectOverlap={true}
+                                eventDurationEditable={false}
+                                eventStartEditable={false}
                                 eventOverlap={false}
-                                expandThrough={false}
+                                // expandThrough={false}
                                 events={events}
-                                dayPropGetter={calendarStyle}
+                                eventMinHeight={66}
+                                eventMinWidth={1000}
+                                dateClick={showModalBooking}
+                                eventClick={showModalBooking}
+                                // dayPropGetter={calendarStyle}
                                 expandRows={true}
                                 slotMinTime={"08:00:00"}
                                 slotMaxTime={"19:00:00"}
-                                slotDuration={"01:00:00"}
+                                slotDuration={"00:30:00"}
                                 slotLabelInterval={{ hours: 1 }}
                                 slotLabelFormat={{
                                     hour: 'numeric',
