8cfcad211069cffc626233cf6987bce1ef1e4c17
trungdq2407 <trungdq2407@gmail.com>
2024-01-19 13:40:29 +0700

    css header

--- src/app/[locale]/(logged-in)/room/page.tsx
+++ src/app/[locale]/(logged-in)/room/page.tsx
@@ -1,10 +1,10 @@
-'use client'
-import styles from '@/css/CompanyList.module.css'
-import {Table, Tag, Pagination } from 'antd';
+"use client";
+import styles from "@/css/CompanyList.module.css";
+import { Table, Tag, Pagination } from "antd";
 import Button from "@/constants/Form/Button";
-import type { DatePickerProps } from 'antd';
-import { DatePicker, Space } from 'antd';
-import customstyle from '@/css/MeetingRoomList.module.css';
+import type { DatePickerProps } from "antd";
+import { DatePicker, Space } from "antd";
+import customstyle from "@/css/MeetingRoomList.module.css";
 import CustomTimePicker from "@/components/Manager/TimePicker";
 import type { ColumnsType } from 'antd/es/table';
 import api from '@/axiosService';
@@ -12,12 +12,15 @@ import { useEffect, useState, useCallback } from "react";
 import AddNewRoom from '@/components/Room/createMeetingRoomModal';
 import { get } from "lodash";
 import toast from "react-hot-toast";
-import DeleteMeeting from '@/components/DeleteMeeting/DeleteMeeting';
-import EditRoom from '@/components/Room/EditMeetingRoomModal';
-import {useSelector} from 'react-redux';
-import moment from 'moment';
+import DeleteMeeting from "@/components/DeleteMeeting/DeleteMeeting";
+import EditRoom from "@/components/Room/EditMeetingRoomModal";
+import { useSelector } from "react-redux";
+import moment from "moment";
+import Link from "next/link";
+import { useLocale } from "next-intl";
 
 const CompanyList = () => {
+  const locale = useLocale();
   const [allRoomsData, setAllRoomData] = useState<DataType[]>([]);
   const [deleteConfirmationVisible, setDeleteConfirmationVisible] = useState(false);
   const [filteredRooms, setFilteredRooms] = useState<DataType[]>([]);
