0ede259683c553228332bef46f72d1ef16e4ad6d
trungdq2407 <trungdq2407@gmail.com>
2024-01-22 09:59:31 +0700

    middleware for url access

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,47 +1,74 @@
 import createIntlMiddleware from "next-intl/middleware";
-import { NextRequest } from "next/server";
+import {NextRequest} from "next/server";
 
 export default async function middleware(request: NextRequest) {
-  const [, locale, ...segments] = request.nextUrl.pathname.split("/");
+    const [, locale, ...segments] = request.nextUrl.pathname.split("/");
 
-  const authRoutes = [
-    "/login",
-    "register",
-    "forgotpassword",
-    "resetpassword",
-    "manager",
-    "guest",
-  ];
-  const adminRoutes = [
+    const authRoutes = [
+        "/login",
+        "register",
+        "forgotpassword",
+        "resetpassword",
+        "manager",
+        "guest",
+    ];
+    const adminRoutes = [
+        "company",
+    ];
+    const managerRoutes = [
+        "room",
+        "booking",
+        "users",
+    ];
+    const userRoutes = [
+        "room",
+        "booking",
+    ];
 
-  ]
+    const token = request.cookies.get("token")?.value;
+    const type = request.cookies.get("type")?.value;
 
-  const token = request.cookies.get("token")?.value;
-  const type = request.cookies.get("type")?.value
+    const handleI18nRouting = createIntlMiddleware({
+        locales: ["en", "vn"],
+        defaultLocale: "en",
+        localePrefix: "always",
+    });
+    let allowedRoutes = [];
+    switch (type) {
+        case 'admin':
+            allowedRoutes = authRoutes.concat(adminRoutes);
+            break;
+        case 'manager':
+            allowedRoutes = authRoutes.concat(managerRoutes);
+            break;
+        case 'user':
+            allowedRoutes = authRoutes.concat(userRoutes);
+            break;
+        default:
+            // Unknown role, redirect to login
+            request.nextUrl.pathname = `/${locale}/login`;
+            request.cookies.delete("token");
+            return handleI18nRouting(request);
+    }
 
-  const handleI18nRouting = createIntlMiddleware({
-    locales: ["en", "vn"],
-    defaultLocale: "en",
-    localePrefix: "always",
-  });
+    if (
+        !token &&
+        !allowedRoutes.includes(segments[0])
+        // TODO check token expired
+    ) {
+        request.nextUrl.pathname = `/${locale}/login`;
+        request.cookies.delete("token");
+        return handleI18nRouting(request);
+    }
 
-  if (
-    !token &&
-    !authRoutes.includes(segments[0])
-    // TODO check token expired
-  ) {
-    request.nextUrl.pathname = `/${locale}/login`;
-    request.cookies.delete("token");
-  }
+    const response = await handleI18nRouting(request);
 
-  const response = handleI18nRouting(request);
-
-  if (!token) {
-    response.cookies.delete("token");
-  }
-  return response;
+    if (!token) {
+        response.cookies.delete("token");
+    }
+    return response;
 }
 
 export const config = {
-  matcher: ["/((?!api|_next|_vercel|.*\\..*).*)", "/([\\w-]+)?/users/(.+)"],
+    matcher: ["/((?!api|_next|_vercel|.*\\..*).*)", "/([\\w-]+)?/users/(.+)"],
 };
