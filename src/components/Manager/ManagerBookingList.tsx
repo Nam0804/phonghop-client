663685450def7687cf6fd0cb2facee941604f730
Trọng Lịch Đoàn <liam@vitex.asia>
2024-01-29 16:35:14 +0700

    update a little bit

--- src/components/Manager/ManagerBookingList.tsx
+++ src/components/Manager/ManagerBookingList.tsx
@@ -13,6 +13,7 @@ import toast from "react-hot-toast";
 import BookingDetail from "./Booking/BookingDetail";
 import BookingEditDetail from "./Booking/BookingEditDetail";
 import { useSelector } from "react-redux";
+import BookingSuccess from "../Booking/BookingSuccess";
 
 const ManagerBookingList = () => {
   const [allStaffData, setAllStaffData] = useState<DataType[]>([]);
@@ -394,6 +395,9 @@ const ManagerBookingList = () => {
       </div>
       <div className={styles.addco}>
         <BookRoom onAddSuccess={handleAddSuccess}></BookRoom>
+      </div>
+      <div className={styles.addco}>
+        <BookingSuccess></BookingSuccess>
       </div>
     </div>
   );
